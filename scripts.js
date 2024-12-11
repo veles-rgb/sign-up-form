71b61875125b28d23b748ba3193874c4cae1a14c
veles-rgb <181009546+veles-rgb@users.noreply.github.com>
2024-12-11 18:07:22 -0500

    Add matching password functionality

--- scripts.js
+++ scripts.js
@@ -1,16 +1,24 @@
-// I attempted to make a script to check that the passwords matched, but could not solve it in time.
+const password = document.getElementById('pwd');
+const confirmPassword = document.getElementById('confirm-password');
+const form = document.getElementById('form');
+const pwdErr = document.querySelector('.form-item span')
 
-// const password = document.getElementById('pwd');
-// const confirmPassword = document.getElementById('confirm-password');
-// const form = document.getElementById('form');
+function validatePasswords() {
+    if (confirmPassword.value != password.value) {
+        confirmPassword.setCustomValidity('Password must match')
+        pwdErr.textContent = "Passwords do not match!";
+    } else {
+        confirmPassword.setCustomValidity('');
+        pwdErr.textContent = "";
+    }
+}
 
-// form.addEventListener('submit', (event) => {
-//     if (confirmPassword.value != password.value) {
-//         password.setCustomValidity('Password must match')
-//         confirmPassword.setCustomValidity('Password must match')
-//         event.preventDefault();
-//     } else {
-//         password.setCustomValidity('');
-//         confirmPassword.setCustomValidity('');
-//     }
-// })
+password.addEventListener('input', validatePasswords)
+confirmPassword.addEventListener('input', validatePasswords)
+
+form.addEventListener('submit', (event) => {
+    validatePasswords()
+    if (!form.checkValidity()) {
+        event.preventDefault();
+    }
+})
