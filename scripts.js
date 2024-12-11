e6ede5b37dcedf18f5833cf6edf1e700f4702aac
veles-rgb <181009546+veles-rgb@users.noreply.github.com>
2024-12-11 16:38:12 -0500

    Add last touches

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// I attempted to make a script to check that the passwords matched, but could not solve it in time.
+
+// const password = document.getElementById('pwd');
+// const confirmPassword = document.getElementById('confirm-password');
+// const form = document.getElementById('form');
+
+// form.addEventListener('submit', (event) => {
+//     if (confirmPassword.value != password.value) {
+//         password.setCustomValidity('Password must match')
+//         confirmPassword.setCustomValidity('Password must match')
+//         event.preventDefault();
+//     } else {
+//         password.setCustomValidity('');
+//         confirmPassword.setCustomValidity('');
+//     }
+// })
